4b1c84f026d88fec3f9625c05db423b6a99e9d50
aryanGajelli <argajelli@gmail.com>
2023-11-20 01:03:15 -0500

    Works very well and quickly with pose

--- Src/color_sensor.c
+++ Src/color_sensor.c
@@ -16,41 +16,12 @@ uint32_t F_CLK;
 #define COLOR_3_EN() HAL_GPIO_WritePin(COLOR_3_CS_GPIO_Port, COLOR_3_CS_Pin, GPIO_PIN_RESET)
 #define COLOR_3_DIS() HAL_GPIO_WritePin(COLOR_3_CS_GPIO_Port, COLOR_3_CS_Pin, GPIO_PIN_SET)
 
-volatile uint8_t isFirstCaptured = 0;
-volatile uint32_t colorSensorPeriod = 0;
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef* htim) {
-    if (0) {
-        static volatile uint32_t colorSensor_T1 = 0;
-        static volatile uint32_t colorSensor_T2 = 0;
-        uint32_t arr = __HAL_TIM_GET_AUTORELOAD(&COLOR_TIMER_HANDLE);
-        if (isFirstCaptured == 0) {
-            colorSensor_T1 = COLOR_TIMER_INSTANCE->CCR1;
-            isFirstCaptured = 1;
-
-        } else {
-            colorSensor_T2 = COLOR_TIMER_INSTANCE->CCR1;
-
-            if (colorSensor_T2 > colorSensor_T1) {
-                colorSensorPeriod = colorSensor_T2 - colorSensor_T1;
-            } else {
-                colorSensorPeriod = (arr - colorSensor_T1) + colorSensor_T2;
-            }
-
-            __HAL_TIM_SET_COUNTER(&COLOR_TIMER_HANDLE, 0);
-            isFirstCaptured = 0;
-            // Stop capturing after recording 1 pulse to prevent saturation of interrupts on the cpu
-            HAL_TIM_Base_Stop_IT(&COLOR_TIMER_HANDLE);
-        }
-    }
-}
-
 HAL_StatusTypeDef colorSensorInit() {
     COLOR_1_DIS();
     COLOR_2_DIS();
     COLOR_3_DIS();
 
     F_CLK = HAL_RCC_GetSysClockFreq();
-    __HAL_TIM_ENABLE_IT(&COLOR_TIMER_HANDLE, TIM_IT_UPDATE);
     if (HAL_TIM_Encoder_Start(&COLOR_TIMER_HANDLE, TIM_CHANNEL_ALL) != HAL_OK) {
         return HAL_ERROR;
     }
@@ -65,7 +36,7 @@ HAL_StatusTypeDef colorSensorInit() {
  */
 uint32_t colorGetFreq(ColorSensor_E sensor) {
     colorSelectSensor(sensor);
-    static const  uint32_t DELAY_TIME_MS = 5;
+    static const uint32_t DELAY_TIME_MS = 2;
     int32_t start = __HAL_TIM_GET_COUNTER(&COLOR_TIMER_HANDLE);
     vTaskDelay(DELAY_TIME_MS);
     int32_t end = __HAL_TIM_GET_COUNTER(&COLOR_TIMER_HANDLE);
@@ -121,6 +92,10 @@ HAL_StatusTypeDef colorSelectSensor(ColorSensor_E sensor) {
  * @param sensor The sensor to get the normalized value of.
  */
 double colorGetNormalizedOut(ColorSensor_E sensor) {
+    // Home Values
+    // static const uint32_t c1_wood = 49500, c2_wood = 66500, c3_wood = 44500;
+    // static const uint32_t c1_tape = 30300, c2_tape = 35200, c3_tape = 26900;
+    // Field Values
     static const uint32_t c1_wood = 65800, c2_wood = 72000, c3_wood = 60500;
     static const uint32_t c1_tape = 26800, c2_tape = 23000, c3_tape = 27000;
     uint32_t freq = colorGetFreq(sensor);
